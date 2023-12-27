cf353c92d88facb5cb7daa4d176688d5bec87a30
Suraj-Encoding <dalvisuraj256@gmail.com>
2023-12-27 17:19:07 +0530

    updated server route

--- Frontend/src/app/components/common/baseUrl.ts
+++ Frontend/src/app/components/common/baseUrl.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://localhost:3000/api";
-// export const BASE_URL = "https://blue-green-llama-hat.cyclic.app/api";
+// export const BASE_URL = "http://localhost:3000/api";
+export const BASE_URL = "https://encoding.onrender.com/api";
